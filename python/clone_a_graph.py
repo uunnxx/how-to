e6cbb8fc78977bf37e19ae81ff737ad7d47b7816
バカです <uunnxx@pm.me>
2023-05-20 18:03:19 +0300

    graph clone
    Signed-off-by: バカです <uunnxx@pm.me>

---
+++ python/clone_a_graph.py
@@ -0,0 +1,73 @@
+from matplotlib.cbook import collections
+import string
+
+
+class GraphVertex:
+    def __init__(self, label):
+        self.label = label
+        self.edges = []
+
+
+def clone_graph(G):
+    if not G:
+        return None
+
+    q = collections.deque([G])
+    vertex_map = {G: GraphVertex(G.label)}
+
+    while q:
+        v = q.popleft()
+        for e in v.edges:
+            # Try to copy vertex e.
+            if e not in vertex_map:
+                vertex_map[e] = GraphVertex(e.label)
+                q.append(e)
+            # Copy edge v -> e
+            vertex_map[v].edges.append(vertex_map[e])
+
+    return vertex_map[G]
+
+
+def is_any_placement_feasible(G):
+    def bfs(s):
+        s.d = 0
+        q = collections.deque([s])
+
+        while q:
+            for t in q[0].edges:
+                if t.d == -1:
+                    t.d = q[0].d + 1
+                    q.append(t)
+                elif t.d == q[0].d:
+                    return False
+
+            del q[0]
+
+        return True
+
+    return all(bfs(v) for v in G if v.d == -1)
+
+
+# Uses BFS to find the least steps of transformation.
+def transform_string(D, s, t):
+    StringWithDistance = collections.namedtuple(
+        'StringWithDistance', ('candidate_string', 'distance')
+    )
+    q = collections.deque([StringWithDistance(s, 0)])
+    D.remove(s)
+
+    while q:
+        f = q.popleft()
+        # Returns if we find a match.
+        if f.candidate_string == t:
+            return f.distance  # Number of steps reaches t.
+
+        # Tries all possible transformations of f.candidate_string.
+        for i in range(len(f.candidate_string)):
+            for c in string.ascii_lowercase:  # Iterates through 'a'..'z'.
+                cand = f.candidate_string[:i] + c + f.candidate_string[i + 1:]
+                if cand in D:
+                    D.remove(cand)
+                    q.append(StringWithDistance(cand, f.distance + 1))
+
+    return -1  # Cannot find a possible transformations.
