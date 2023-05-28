fa801c1dac4bc1b4d7119176c593ca0669859ff7
バカです <uunnxx@pm.me>
2023-05-28 17:21:00 +0300

    updated: `2023-05-28 17:21`
    Signed-off-by: バカです <uunnxx@pm.me>

---
+++ python/objects.py
@@ -0,0 +1,29 @@
+class User:
+    def __init__(self, name, occupation):
+        self.name = name
+        self.occupation = occupation
+
+    def __repr__(self):
+        return f"{self.name=} is a {self.occupation=}"
+
+    def __str__(self):
+        return f"{self.name} is a {self.occupation}"
+
+
+u = User('John Doe', 'gardener')
+
+print(f'{u!r}')
+print(f'{u!s}')
+
+import datetime
+
+
+now = datetime.datetime.now()
+
+print()
+print(f'{now:%Y-%m-%d %H:%M}')
+print(f'{now:%A}')
+print(f'{now:%a}')
+print(f'{now:%B}')
+print(f'{now:%j}')
+print(f'{now:%w}')
