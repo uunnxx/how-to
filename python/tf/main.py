88de8cb92f78687ade802e6ab55cdd963c86eaf1
バカです <naimjonn.nn@live.ru>
2024-05-02 05:23:01 +0300

    deleted

--- python/tf/main.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras import layers
-
-with tf.device('GPU:0'):
-    layer1 = layers.Dense(16, input_dim=8)
-
-with tf.device('GPU:1'):
-    layer2 = layers.Dense(4, input_dim=16)
